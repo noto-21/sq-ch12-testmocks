468deaa044a28d96fba844468e84fab3d38ab2d0
DemiwizAces <erdmriosnadeau@gmail.com>
2024-03-04 16:26:01 -0500

    Changed BankAccountFactory

--- src/main/java/com/sofe3980u/BankingSystem/BankAccountFactory.java
+++ src/main/java/com/sofe3980u/BankingSystem/BankAccountFactory.java
@@ -1,6 +1,14 @@
 package com.sofe3980u.BankingSystem;
 
-public interface BankAccountFactory {
-	Account createAccount();
-
+public class BankAccountFactory {
+	public Account createAccount(String type, String accountName, String accountHolder, double initialBalance) {
+        switch (type.toLowerCase()) {
+            case "savings":
+                return new SavingsAccount(accountName, accountHolder, initialBalance);
+            case "checking":
+                return new CheckingAccount(accountName, accountHolder, initialBalance);
+            default:
+                throw new IllegalArgumentException("Invalid account type: " + type);
+        }
+    }
 }
